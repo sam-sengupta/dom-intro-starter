845695d1eae74a175e3366c9d9d92757e8bec4ef
Sam <ss11976@nyu.edu>
2025-02-25 14:43:36 -0700

    02/25 ICA

---
+++ script.js
@@ -0,0 +1,31 @@
+// one
+document.getElementById("firstPara").innerHTML += "<span class='tan'> This is the new tan text.</span>";
+
+// two
+let images = document.getElementsByTagName("img");
+for (let image of images) {
+    image.width = "250";
+}
+
+// three
+let lightBlueSpans = document.getElementsByClassName("light-blue");
+let dkBlueSpans = document.getElementsByClassName("med-blue");
+
+for (let span of lightBlueSpans) {
+    span.style.color = "#7C9EA6";
+}
+
+for (let span of dkBlueSpans) {
+    span.style.color = "#3C5E73";
+}
+
+// four
+let svgs = document.querySelectorAll("#svgs svg");
+
+let colors = ("#283040", "#3C5E73", "#7C9EA6", "#D9BCA3", "#F2DCC9");
+for (let i = 0; i < svgs.length; i++) {
+    svgs[i].style.stroke = colors[i];
+}
+
+// five
+document.querySelector("#query .bold").style.color = "#7C9EA6";
